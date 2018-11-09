4c8c34b750aa3a351cc99dfd1af6f6dd8c70d04b
Altos <druss.altos@yandex.ru>
2018-11-09 16:17:35 +0400

    init

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,9 +15,13 @@ mix.styles([
     'resources/assets/admin/bootstrap/css/bootstrap.min.css',
     'resources/assets/admin/font-awesome/4.5.0/css/font-awesome.min.css',
     'resources/assets/admin/ionicons/2.0.1/css/ionicons.min.css',
-    'resources/assets/admin/css/AdminLTE.min.css',
-    'resources/assets/admin/css/skins/_all-skins.min.css'
-], 'public/css/admin.css')
+    'resources/assets/admin/dist/css/AdminLTE.min.css',
+    'resources/assets/admin/dist/css/skins/_all-skins.min.css',
+    'resources/assets/admin/plugins/iCheck/all.css',
+    'resources/assets/admin/plugins/datepicker/datepicker3.css',
+    'resources/assets/admin/plugins/select2/select2.min.css',
+    'resources/assets/admin/plugins/datatables/dataTables.bootstrap.css'
+], 'public/css/admin.css');
 
 mix.scripts([
     'resources/assets/admin/plugins/jQuery/jquery-2.2.3.min.js',
@@ -25,5 +29,14 @@ mix.scripts([
     'resources/assets/admin/plugins/slimScroll/jquery.slimscroll.min.js',
     'resources/assets/admin/plugins/fastclick/fastclick.js',
     'resources/assets/admin/dist/js/app.min.js',
-    'resources/assets/admin/dist/js/demo.js'
-], 'public/js/admin.js')
+    'resources/assets/admin/dist/js/demo.js',
+    'resources/assets/admin/plugins/select2/select2.full.min.js',
+    'resources/assets/admin/plugins/datepicker/bootstrap-datepicker.js',
+    'resources/assets/admin/lugins/iCheck/icheck.min.js',
+    'resources/assets/admin/plugins/datatables/jquery.dataTables.min.js',
+    'resources/assets/admin/plugins/datatables/dataTables.bootstrap.min.js',
+    'resources/assets/admin/dist/js/script.js',
+], 'public/js/admin.js');
+
+mix.copy('resources/assets/admin/bootstrap/fonts', 'public/fonts');
+mix.copy('resources/assets/admin/dist/fonts', 'public/fonts');
